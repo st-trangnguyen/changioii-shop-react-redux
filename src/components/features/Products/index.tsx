4f5c08055b54cbc22b89541330b54a90643fad02
Trang Nguyen T <trang.nguyen@supremetech.vn>
2020-06-19 16:30:58 +0700

    Add basic folder

---
+++ src/components/features/Products/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header, Footer } from '../../shared';
+
+export const Products = () => {
+  return(
+    <div>
+      <Header />
+      <main>Main content</main>
+      <Footer />
+    </div>
+  )
+}
---
+++ src/containers/Products.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Products } from '../components/features/Products';
+
+export const ProductsContainer = () => {
+  return <Products />
+}
