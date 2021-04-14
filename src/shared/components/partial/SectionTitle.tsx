cc1c19ab82225d33147680fb67b6a248562c8f71
Trang Nguyen T <trang.nguyen@supremetech.vn>
2021-04-14 21:45:44 +0700

    Start config CI

--- src/shared/components/partial/SectionTitle.tsx
+++ src/shared/components/partial/SectionTitle.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
-declare namespace SectionTitle {
-  interface Props {
-    mainTitle: string,
-    subTitle: string
-  }
-}
-
-const SectionTitle = (title: SectionTitle.Props) => {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const SectionTitle = (title: any) => {
   return(
     <div className="section-title text-center pd-5">
       <h2 className="title mb-5 txt-xxl">{ title.mainTitle }</h2>
       <h4 className="sub-title txt-lg txt-secondary pt-5">{ title.subTitle }</h4>
     </div>
 
-  )
+  );
 };
 
 export default SectionTitle;
