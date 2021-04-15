a76f07f3737bc3b7394307ef9188fae9bbf7aba0
Trang Nguyen T <trang.nguyen@supremetech.vn>
2021-04-15 13:14:19 +0700

    Test get pull request

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import { Categories } from './shared/containers/categories';
 const PageRoutes = React.lazy(() => import('./pages/PageRoutes'));
-const AuthRoutes = React.lazy(() => import('./auth/AuthRoutes'));
+const AuthRoutes = React.lazy(() => import('./auth/AuthRoutes'))
 
 export default function Routes() {
-  const token = localStorage.getItem('ACCESS-TOKEN');
   /* if else for Features or Auth layout appears */
   return (
     <div className="wrapper">
